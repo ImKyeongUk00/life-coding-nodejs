ac626456641d7b36451d2649bb04a0725a6acef2
ImKyeongUk <imkyeonguk00@gmail.com>
2020-04-14 20:34:24 +0900

    다중 사용자 시스템 도입, bcrypt로 비밀번호 암호화

--- routes/auth.js
+++ routes/auth.js
@@ -2,38 +2,41 @@ var express = require('express');
 var router = express.Router();
 var template = require('../lib/template.js');
 var auth = require('../lib/auth.js');
+var db = require('../lib/db.js');
+var errorControl = require('../lib/error.js');
+var bcrypt = require('bcrypt');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-
-const userData = { //단일 사용자의 계정, 비밀번호, 닉네임 정보
-    email :'imkyeonguk00@gmail.com',
-    password : '000000',
-    nicname : 'devUk'
-};
 
 //passport의 local 전략 사용
 passport.use(new LocalStrategy({
   usernameField: 'email',
   passwordField: 'password'
 },
-function(username, password, done) {
-  if(username === userData.email){
-    if(password === userData.password){
-      return done(null, userData);
+function(email, password, done) {
+  db.query(`SELECT * FROM users WHERE email=?`, [email], function(error, user){
+    if(user[0] === undefined){//없는 email
+      return done(null, false, { message: 'Incorrect email.' });
     }else{
-      return done(null, false, { message: 'Incorrect password.' });
-    }
-  }else{
-    return done(null, false, { message: 'Incorrect email.' });
-  }
+      bcrypt.compare(password, user[0].password, function(err, result) {
+        if(result !== true){
+          return done(null, false, { message: 'Incorrect password.' });
+        }else{
+          return done(null, user[0]);
+        }
+      });
+    }  
+  });
 }));
 
 passport.serializeUser(function(user, done) {//login에 성공하면 그 때 딱 한 번 실행되어 session store에 session data를 저장해준다.
-  done(null, user.email);
+  done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {//page에 접근 할 때마다 실행되어 사용자의 정보를 넘겨준다.
-  done(null, userData);
+passport.deserializeUser(function(id, done) {//page에 접근 할 때마다 실행되어 사용자의 정보를 넘겨준다. request.user객체가 여기서 생성될듯
+  db.query(`SELECT * FROM users WHERE id=?`, [id], function(error, user){
+    done(null, user[0]);
+  });
 });
 
 router.get('/login', function(request, response){
@@ -68,6 +71,49 @@ router.get('/logout_process', function(request, response, next){
     }else{
         response.redirect('/auth/login');
     }
+});
+
+router.get('/register', function(request, response){
+  var title = 'Register';
+  var list = template.list(request.topics);
+  var body = `
+      <h2>${title}</h2>
+      <form action="/auth/register_process" method="post">
+          <p><input type="text" name="email" placeholder="email"></p>
+          <p><input type="password" name="password" placeholder="password"></p>
+          <p><input type="password" name="password2" placeholder="password again"></p>
+          <p><input type="text" name="nicname" placeholder="nicname"></p>
+          <p><input type="submit" value="register"></p>
+      </form>
+    `;
+    var control = ``;
+    var html = template.HTML(title, list, body, control, auth.statusUI(request));
+    response.send(html);
+});
+
+router.post('/register_process', function(request, response, next){
+  db.query(`SELECT * FROM users WHERE email=?`, [request.body.email], function(error, user){
+    errorControl.throw(error, next);
+    
+    if(user[0] === undefined){//이메일 중복이 아니면
+      if(request.body.password !== request.body.password2){
+        response.send('<h1>비밀번호 에러</h1>');
+      }else{
+        bcrypt.hash(request.body.password, 10, function(error1, hash) {
+          errorControl.throw(error1, next);
+
+          db.query(`INSERT INTO users (email, password, nicname) VALUES (?, ?, ?)`, 
+          [request.body.email, hash, request.body.nicname],
+          function(error2){
+            errorControl.throw(error2, next);
+            response.redirect('/auth/login');
+          });  
+        });
+      }
+    }else{
+      response.send('<h1>이메일 중복</h1>');
+    }
+  });
 });
 
 module.exports = router;
--- routes/topic.js
+++ routes/topic.js
@@ -35,7 +35,8 @@ router.post('/create_process', function(request, response, next){
         var title = request.body.title;
         var description = request.body.description;
         var author_id = request.body.author_id;
-        db.query(`INSERT INTO topic (title, description, created, author_id) VALUES (?, ?, NOW(), ?)`, [title, description, author_id], 
+        db.query(`INSERT INTO topic (title, description, created, author_id, user_id) VALUES (?, ?, NOW(), ?, ?)`, 
+            [title, description, author_id, request.user.id], 
             function(error, results){
             errorControl.throw(error, next);
             response.redirect(`/topic/${results.insertId}`);
@@ -49,24 +50,28 @@ router.get('/update/:pageID', function(request, response, next){
     if(auth.is_login(request)){//로그인 상태라면
         db.query(`SELECT * FROM topic WHERE id = ?`, [request.params.pageID], function(error, topic){
             errorControl.throw(error, next);
-            var title = topic[0].title;
-            var list = template.list(request.topics);
-            var control = `<a href="/create">create</a> <a href="/update?id=${request.params.pageID}">update</a>`;
-            var body = `
-                <h2>Update topic</h2>
-                <form action="/topic/update_process" method="post">
-                    <input type="hidden" name="id" value="${topic[0].id}">
-                    <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
-                    <p>
-                    <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-                    </p>
-                    <p>
-                    <input type="submit" value="update">
-                    </p>
-                </form>
-            `;
-            var html = template.HTML(title, list, body, control, auth.statusUI(request));
-            response.send(html);
+            if(auth.is_owner(request, topic[0].user_id)){
+                var title = topic[0].title;
+                var list = template.list(request.topics);
+                var control = `<a href="/create">create</a> <a href="/update?id=${request.params.pageID}">update</a>`;
+                var body = `
+                    <h2>Update topic</h2>
+                    <form action="/topic/update_process" method="post">
+                        <input type="hidden" name="id" value="${topic[0].id}">
+                        <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+                        <p>
+                            <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+                        </p>
+                        <p>
+                            <input type="submit" value="update">
+                        </p>
+                    </form>
+                `;
+                var html = template.HTML(title, list, body, control, auth.statusUI(request));
+                response.send(html);
+            }else{
+                response.redirect('/');
+            }
         });
     }else{
         response.redirect('/auth/login');
@@ -75,12 +80,18 @@ router.get('/update/:pageID', function(request, response, next){
 
 router.post('/update_process', function(request, response, next){
     if(auth.is_login(request)){//로그인 상태라면
-        var id = request.body.id;
-        var title = request.body.title;
-        var description = request.body.description;
-        db.query(`UPDATE topic SET title=?, description=?, created=NOW() WHERE id = ?`, [title, description, id], function(error, result){
-            errorControl.throw(error, next);
-            response.redirect(`/topic/${id}`);
+        db.query('SELECT * FROM topic WHERE id = ?', [request.body.id], function(error, topic){
+            if(auth.is_owner(request, topic[0].user_id)){
+                var id = request.body.id;
+                var title = request.body.title;
+                var description = request.body.description;
+                db.query(`UPDATE topic SET title=?, description=?, created=NOW() WHERE id = ?`, [title, description, id], function(error, result){
+                    errorControl.throw(error, next);
+                    response.redirect(`/topic/${id}`);
+                });
+            }else{
+                response.redirect('/');
+            }
         });
     }else{
         response.redirect('/auth/login');
@@ -89,10 +100,16 @@ router.post('/update_process', function(request, response, next){
 
 router.post('/delete_process', function(request, response, next){
     if(auth.is_login(request)){//로그인 상태라면
-        var id = request.body.id;
-        db.query(`DELETE FROM topic WHERE id=?`, [id], function(error, result){
-            errorControl.throw(error, next);
-            response.redirect('/');
+        db.query('SELECT * FROM topic WHERE id = ?', [request.body.id], function(error, topic){
+            if(auth.is_owner(request, topic[0].user_id)){
+                var id = request.body.id;
+                db.query(`DELETE FROM topic WHERE id=?`, [id], function(error, result){
+                    errorControl.throw(error, next);
+                    response.redirect('/');
+                });
+            }else{
+                response.redirect('/');
+            }
         });
     }else{
         response.redirect('/auth/login');
@@ -103,21 +120,25 @@ router.get('/:pageID', function(request, response, next){
     if(auth.is_login(request)){//로그인 상태라면
         db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id = ?`, [request.params.pageID], function(error, topic){
             errorControl.throw(error, next);
-            var sanitizedTitle = sanitizeHtml(topic[0].title);
-            var sanitizedDescription = sanitizeHtml(topic[0].description);
-            var sanitizedAuthorName = sanitizeHtml(topic[0].name);
-            var list = template.list(request.topics);
-            var body = `<h2>${sanitizedTitle}</h2>${sanitizedDescription} <p>by ${sanitizedAuthorName}</p>`;
-            var control = ` 
-                <a href="/topic/create">create</a>
-                <a href="/topic/update/${request.params.pageID}">update</a>
-                <form action="/topic/delete_process" method="post">
-                    <input type="hidden" name="id" value="${request.params.pageID}">
-                    <input type="submit" value="delete">
-                </form>
-            `;
-            var html = template.HTML(sanitizedTitle, list, body, control, auth.statusUI(request));
-            response.send(html);
+            if(auth.is_owner(request, topic[0].user_id)){
+                var sanitizedTitle = sanitizeHtml(topic[0].title);
+                var sanitizedDescription = sanitizeHtml(topic[0].description);
+                var sanitizedAuthorName = sanitizeHtml(topic[0].name);
+                var list = template.list(request.topics);
+                var body = `<h2>${sanitizedTitle}</h2>${sanitizedDescription} <p>by ${sanitizedAuthorName}</p>`;
+                var control = ` 
+                    <a href="/topic/create">create</a>
+                    <a href="/topic/update/${request.params.pageID}">update</a>
+                    <form action="/topic/delete_process" method="post">
+                        <input type="hidden" name="id" value="${request.params.pageID}">
+                        <input type="submit" value="delete">
+                    </form>
+                `;
+                var html = template.HTML(sanitizedTitle, list, body, control, auth.statusUI(request));
+                response.send(html);
+            }else{
+                response.redirect('/');
+            }
         }); 
     }else{
         response.redirect('/auth/login');
